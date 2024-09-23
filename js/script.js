151d02661bc04a7838fc5b861595a70024a93a6b
Fidann12 <fidan.aliyeva03@icloud.com>
2024-09-23 21:40:34 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,56 @@
+// 1) Verilmiş n - ədədinin 3-ə və 7-ə bölünüb-bölünməməsini tapan bir function yazin.
+
+// let number = n;
+// if (number % 3 == 0 && number%7==0) {
+//   console.log("eded 3e ve 7e qaliqsiz bolunur");
+// } else {
+//   console.log("3e ve 7e bolunmur");
+// }
+
+// 2) Functiona musbet bir eded gelir, hemin funksiya gelen ededin faktorialini hesablayib geri qaytarmalidir.
+
+// let number = n;
+// let result = 1;
+
+// if (number > 0) {
+//   for (let i = 1; i <= number; i++) {
+//     result *= i;
+//   }
+//   console.log(result);
+// } else {
+//   console.log("musbet eded deyil");
+// }
+
+// 3) Her hansisa number elementleri olan arayin icindeki cut ededlerin kvadratlarinin cemini hesablayan bir function yazin.
+
+// let array = [n, n1, n2, n3];
+// let sum = 0;
+
+// for (let i = 0; i < array.length; i++) {
+//   if (array[i] % 2 == 0) {
+//     sum += array[i] ** 2;
+//   }
+// }
+// console.log(sum);
+
+// 4) Funtiona-a  mail ve password gelir. Mail beraberdirse cavid@code.edu.az -a ve password beraberdirse 12345 -e, ekranda "Girish olundu" yazilsin, eks halda "Mail ve yaxud password sehvdir" yazilsin.
+
+// let mail;
+// let password;
+
+// if (mail == "cavid@code.edu.az" && password == "12345") {
+//   console.log("girish oldu");
+// } else {
+//   console.log("Mail ve yaxud password sehvdir");
+// }
+
+// 6) Functiona ededlerden ibaret array gelir, function hemin arraydaki cut ededlerin sayini qaytarmalidir.
+
+// let array = [n, n1.n2.n3];
+// let count = 0;
+// for (let i = 0; i < array.length; i++) {
+//   if (array[i] % 2 == 0) {
+//     count++;
+//   }
+// }
+// console.log(count);
